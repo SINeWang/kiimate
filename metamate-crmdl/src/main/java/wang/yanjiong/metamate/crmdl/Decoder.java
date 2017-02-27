55f796277006537772e0c75ccb1f70b035124dd5
SINeWang <im.sinewang@gmail.com>
2017-02-27 14:17:46 +0800

    rename lang as crmdl

---
+++ metamate-crmdl/src/main/java/wang/yanjiong/metamate/crmdl/Decoder.java
@@ -0,0 +1,9 @@
+package wang.yanjiong.metamate.crmdl;
+
+/**
+ * Created by WangYanJiong on 2/27/17.
+ */
+public class Decoder {
+
+
+}
---
+++ metamate-crmdl/src/test/java/wang/yanjiong/metamate/crmdl/TestDecoder.java
@@ -0,0 +1,7 @@
+package wang.yanjiong.metamate.crmdl;
+
+/**
+ * Created by WangYanJiong on 2/27/17.
+ */
+public class TestDecoder {
+}
